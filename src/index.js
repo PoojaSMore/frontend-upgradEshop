1af03ae9ccde8dfaf3a46aadc5fdf6b81c3dc81e
PoojaSMore <poojamore056@gmail.com>
2022-04-26 20:48:03 +0530

    AddProducts, ProductCard, ProductDetails, Products components created

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NavigationBar from './components/NavigationBar';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import SignUpPage from './components/SignUp';
-import SignInPage from './components/SignIn';
-import AddProductsPage from './components/AddProducts';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import SignUpPage from './components/SignUp/SignUp';
+import SignInPage from './components/SignIn/SignIn';
+import AddProductsPage from './components/AddProducts/AddProducts';
+import ProductCard from './components/ProductCard/ProductCard';
+import Products from './components/Products/Products';
+import ProductDetails from './components/ProductDetails/ProductDetails';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,6 +20,9 @@ ReactDOM.render(
             <Route path='/login' element={ <SignInPage /> }></Route>
             <Route path='/signup' element={ <SignUpPage /> }></Route>
             <Route path='/addproducts' element={ <AddProductsPage /> }></Route>
+            <Route path='/productcard' element={ <ProductCard/> }></Route>
+            <Route path='/products' element={<Products/> }></Route>
+            <Route path='/productdetails' element={<ProductDetails/>}></Route>
           </Routes>
         </section>
       </BrowserRouter>
